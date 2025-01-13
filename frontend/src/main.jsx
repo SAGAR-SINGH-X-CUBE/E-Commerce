48259426ce7dd33eb8aafa43bb520d1221983b4d
SAGAR-SINGH-X-CUBE <sagarsinghxcube@gmail.com>
2025-01-13 22:17:25 +0530

    second changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,11 @@
-
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-
+import './index.css'  // Importing global styles
+import App from './App.jsx'  // Your main app component
+import { BrowserRouter } from 'react-router-dom'  // Importing BrowserRouter for routing
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <App />  {/* App component is wrapped inside BrowserRouter */}
   </BrowserRouter>,
 )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const Home = () => {
   return (
     <div>
-      
+      <h1>Welcome to Home Page</h1> {/* Test if Home page renders */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
